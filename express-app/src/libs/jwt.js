206c49e73bc0b1edf505a05ef10dc89e8d05f0a4
[german salmieri] <[german.salmi@gmail.com]>
2023-11-13 02:45:38 -0300

    user complete, token expires in 2h. heading into items.

--- express-app/src/libs/jwt.js
+++ express-app/src/libs/jwt.js
@@ -3,7 +3,7 @@ import { SECRET_TOKEN } from "../config.js";
 
 export const createAccessToken = (payload) => {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, SECRET_TOKEN, { expiresIn: "1d" }, (err, token) => {
+    jwt.sign(payload, SECRET_TOKEN, { expiresIn: "2h" }, (err, token) => {
       if (err) reject (err)
       resolve(token);
     });
